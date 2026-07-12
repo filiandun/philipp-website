0fa9270c6ca8076e5e79543999ae32ba5bfd6195
filiandun <117369784+filiandun@users.noreply.github.com>
2026-07-12 17:36:55 +0000

    Deploying to gh-pages from @ filiandun/philipp-website@5d48f4a2a3d86590668098d9cfc4a887e45d8ed6 🚀

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+﻿window.sectionObserver = {
+    observer: null,
+
+    observe(dotNetRef, selector) {
+        const sections = document.querySelectorAll(selector);
+
+        this.observer = new IntersectionObserver((entries) => {
+            const visible = entries
+                .filter(entry => entry.isIntersecting)
+                .sort((a, b) => b.intersectionRatio - a.intersectionRatio)[0];
+
+            if (visible) {
+                dotNetRef.invokeMethodAsync(
+                    "SetActiveSection",
+                    visible.target.id
+                );
+            }
+        }, {
+            threshold: [0.45, 0.6, 0.75]
+        });
+
+        sections.forEach(section => this.observer.observe(section));
+    },
+
+    disconnect() {
+        this.observer?.disconnect();
+        this.observer = null;
+    }
+};
