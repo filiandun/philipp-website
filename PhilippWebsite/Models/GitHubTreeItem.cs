12bbe182fd57ca2782b8b1eb041bfc1474094c28
filian <117369784+filiandun@users.noreply.github.com>
2026-09-19 21:08:39 +0300

    WIP: add an activitybar with solution explorer, setup github api client

---
+++ PhilippWebsite/Models/GitHubTreeItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+
+namespace PhilippWebsite.Models
+{
+    public class GitHubTreeItem
+    {
+        [JsonPropertyName("path")]
+        public string Path { get; set; } = string.Empty;
+
+
+        [JsonPropertyName("mode")]
+        public string Mode { get; set; } = string.Empty;
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = string.Empty;
+
+
+        [JsonPropertyName("sha")]
+        public string Sha { get; set; } = string.Empty;
+
+        [JsonPropertyName("size")]
+        public int? Size { get; set; }
+
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; } = string.Empty;
+    }
+}
